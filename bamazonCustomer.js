f58bdfae9eb50414f949445dac339a3593d53e2d
ashleyxwang <ashleywang514@yahoo.com>
2018-11-29 17:45:00 -0600

    tidy up finished product

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,8 +26,8 @@ function displayProducts() {
         function (err, res) {
             if (err) throw err;
             let result = JSON.parse(JSON.stringify(res));
-            let resultArray = result.map(input => [input.item_id, input.product_name, input.price.toFixed(2)]);
-            resultArray.unshift(['ID', 'PRODUCT', 'PRICE']);
+            let resultArray = result.map(input => [input.item_id, input.product_name, input.price.toFixed(2), input.stock_quantity]);
+            resultArray.unshift(['ID ', 'PRODUCT ', 'PRICE  ', 'QTY  ']);
 
             const display = table(resultArray);
 
@@ -36,7 +36,7 @@ function displayProducts() {
             //allow customer to place order
             purchaseOrder(res);
         }
-    )
+    );
 }
 
 function purchaseOrder(res) {
@@ -46,20 +46,21 @@ function purchaseOrder(res) {
         {
             name: "productChoice",
             message: "What is the ID of the product you would like to purchase? (Choose one!)",
-            validate: function (productID) { 
+            validate: function (productID) {
                 product = res.find(database => Number(productID) === database.item_id);
+
                 // console.log("validating product:", product); 
                 if (product === undefined || product === NaN) {
                     return "I don't know that product ID.";
                 } else return true
             }
-        }, 
+        },
         {
             name: "productAmount",
             message: "How many of this product would you like to purchase?",
             validate: function (productQuantity) {
                 quantity = Number(productQuantity);
-                let quantityInStock = product.stock_quantity; 
+                let quantityInStock = product.stock_quantity;
                 if (!Number.isInteger(quantity) || quantity <= 0) {
                     return "Invalid quantity"
                 } else if (quantity > quantityInStock) {
@@ -68,91 +69,26 @@ function purchaseOrder(res) {
             }
         }
     ]).then(answers => {
-        let productPrice = quantity * product.price;
-        console.log("You're good to go! The price is: " + productPrice.toFixed(2));
-        // update the database to reduce the quantitesssssss =D 
-
         connection.query(
             "UPDATE products SET ? WHERE ?",
             [
                 {
                     stock_quantity: product.stock_quantity - quantity
-                }, 
+                },
                 {
                     item_id: product.item_id
                 }
             ],
             function (err, res) {
                 if (err) throw err;
+
                 console.log(res.affectedRows + " products updated!\n");
-                // let result = JSON.parse(JSON.stringify(res));
-                // let resultArray = result.map(input => [input.item_id, input.product_name, input.price.toFixed(2)]);
-                // resultArray.unshift(['ID', 'PRODUCT', 'PRICE']);
-    
-                // const display = table(resultArray);
-    
-                // //show available products
-                // console.log(display);
-                // //allow customer to place order
-                // purchaseOrder(res);
-            }                
-        );        
-        
-        connection.end();
+
+                let productPrice = quantity * product.price;
+                console.log("The total price of your transaction is: " + productPrice.toFixed(2));
+
+                connection.end();
+            }
+        );
     });
 }
-
-// UPDATE table_name
-// SET column1 = value1, column2 = value2, ...
-// WHERE condition;
-
-// var query = connection.query(
-//     "UPDATE products SET ? WHERE ?",
-//     [
-//       {
-//         quantity: 100
-//       },
-//       {
-//         flavor: "Rocky Road"
-//       }
-//     ],
-//     function(err, res) {
-//       console.log(res.affectedRows + " products updated!\n");
-//       // Call deleteProduct AFTER the UPDATE completes
-//       deleteProduct();
-//     }
-//   );
-
-
-        // let product;
-        // for(let i=0; i < res.length; i++) {
-        //     if (res[i].item_id === productID) {
-        //         product = res[i];
-        //         break;
-        //     }
-        // }
-        // // make it a function that we can change, if we want to
-        // function isthatwhatIWant(whatyougotforme){
-        //     return whatyougotforme.item_id === productID;
-        // }
-
-        // let product;
-        // for(let i=0; i < res.length; i++) {
-        //     if (isthatwhatIWant(res[i].item_id)) {
-        //         product = res[i];
-        //         break;
-        //     }
-        // }
-        // // ORRRR
-        // let product = res.find(isthatwhatIWant);
-        // // ORRR we can define that function directly into the find
-        // let product = res.find(function(whatyougotforme) {
-        //     return productID === whatyougotforme.item_id
-        // });
-        // // ORRR turn it into a fat-arrow
-        // let product = res.find(whatyougotforme => {
-        //     return productID === whatyougotforme.item_id
-        // })        
-        // // and replace the return
-        // let product = res.find(whatyougotforme => productID === whatyougotforme.item_id)
-        // // the above is equivalent
